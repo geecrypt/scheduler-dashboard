485cd0b15d9aedfeef06328032e88dffea0585d3
geecrypt <gavina352@gmail.com>
2021-09-09 17:03:17 -0700

    create panels component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,9 +2,37 @@ import React, {Component} from "react";
 
 import classnames from "classnames";
 import Loading from "./Loading";
+import Panel from "./Panel";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
 
 class Dashboard extends Component {
-  state = {loading: true}
+  state = {
+    loading: false,
+    focused: null
+  }
+
   render() {
     const dashboardClasses = classnames("dashboard");
 
@@ -12,8 +40,23 @@ class Dashboard extends Component {
       return <Loading />;
     }
 
+    const panels = data
+      .filter(panel => this.state.focused === null || this.state.focused === panel.id)
+      .map(panel => {
+        return (
+          <Panel
+            key={panel.id}
+            id={panel.id}
+            label={panel.label}
+            value={panel.value}
+          />
+        );
+      });
+
     return (
-      <main className={dashboardClasses} />
+      <main className={dashboardClasses}>
+        {panels}
+      </main>
     );
   }
 }
